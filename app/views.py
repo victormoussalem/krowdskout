16962fb082cdde60edd9aec8062aff26ad37edc7
Victor <victor.moussalem@gmail.com>
2015-07-24 11:29:07 -0700

    changed view

--- app/views.py
+++ app/views.py
@@ -33,6 +33,9 @@ def refresh_locations():
 # Also have access to data, published_at, and event json parameters.  Data is duplicate of occ_change.
 @app.route('/update_occupancy', methods=['Post'])
 def update_occupancy():
+	l = Location.query.filter_by(device_id=device_id).first()
+	l.occupancy_count = occ_change
+	db.session.commit()
 #	data = request.get_json(force=True)
 #	if data is not None:
 #		device_id = data['coreid']
