3ac2dbdf5533e9e62cdae4ef00f8e852ed03f1fe
Victor <victor.moussalem@gmail.com>
2015-07-28 01:08:13 -0700

    reverted views

--- app/views.py
+++ app/views.py
@@ -33,21 +33,21 @@ def refresh_locations():
 # Also have access to data, published_at, and event json parameters.  Data is duplicate of occ_change.
 @app.route('/update_occupancy', methods=['Post'])
 def update_occupancy():
-	data = request.get_json(force=True)
-	#device_id = data['coreid']
-	#occ_change = data['occ_change']
-	l = Location.query.first()
-	l.occupancy_count += 3
-	db.session.commit()
 #	data = request.get_json(force=True)
-#	if data is not None:
-#		device_id = data['coreid']
-#		if device_id is not None:
-#			occ_change = data['occ_change']
-#			if occ_change is not None:
-#				l = Location.query.filter_by(device_id=device_id).first()
-#				if l is not None:
-#					l.occupancy_count += occ_change
-#					db.session.commit()
-#					return 'Success!'
+#	#device_id = data['coreid']
+#	#occ_change = data['occ_change']
+#	l = Location.query.first()
+#	l.occupancy_count += 3
+#	db.session.commit()
+	data = request.get_json(force=True)
+	if data is not None:
+		device_id = data['coreid']
+		if device_id is not None:
+			occ_change = data['occ_change']
+			if occ_change is not None:
+				l = Location.query.filter_by(device_id=device_id).first()
+				if l is not None:
+					l.occupancy_count += occ_change
+					db.session.commit()
+					return 'Success!'
 	return 'Failure :('
