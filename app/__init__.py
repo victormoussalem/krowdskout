f3400ca496927375bcb8999e700a282fa53daaf3
Victor <victor.moussalem@gmail.com>
2015-08-11 22:06:21 -0700

    path change

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+# setting up template directory
+ASSETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static/app')
+
+app = Flask(__name__, template_folder=ASSETS_DIR, static_folder=ASSETS_DIR)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
